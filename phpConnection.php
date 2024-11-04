ba02698bb821cadc777fd6fde01328dee26b2c47
Mantech-co <hello2manish@outlook.com>
2024-11-04 16:36:33 +0530

    Add files via upload

---
+++ phpConnection.php
@@ -0,0 +1,16 @@
+<?php
+include 'connect.php';
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
